45ed559074107118d3a343a23f219a19bf398f37
SBars <samuelbarsuhn@gmail.com>
2023-03-15 15:45:21 -0500

    finished the routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -24,6 +24,19 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Category, {
+      model: Tag, 
+      through: ProductTag,
+    }]
+  }).then(data => {
+    res.status(200).json(data)
+  }).catch(err => {
+    res.status(400).json(err)
+  })
 });
 
 // create new product
@@ -102,6 +115,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+    res.status(200).json(data)
+  }).catch(err => {
+    res.status(400).json(err)
+  })
 });
 
 module.exports = router;
